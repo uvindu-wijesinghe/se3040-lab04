3a2ee896734c7b6985da600ba68116865c70b7b2
Uvindu <vidvangauvindu@email.com>
2026-02-21 18:15:19 +0530

    done

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const myPromise = new Promise((resolve, reject) => {
   }
 });
 
-myPromise
-  .then((result) => {
+async function myFunction() {
+  try {
+    const result = await myPromise;
     console.log(result);
-  })
-  .catch((error) => {
+  } catch (error) {
     console.log(error);
-  });
+  }
+}
+
+myFunction();
